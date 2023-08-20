8013b105de5b5dc3080653f0978a4fc62aa19621
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-08-20 13:19:44 +0300

    sending response from the server.

--- Nodejs Course/1- Basics/app.js
+++ Nodejs Course/1- Basics/app.js
@@ -3,7 +3,13 @@ const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    //console.log(req);
+
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Server Response HTML</title></head>");
+    res.write("<body><h1>Hello, I'm the server. How are you doing?</h1></body>");
+    res.write("</html>");
 });
 
 server.listen(port);
